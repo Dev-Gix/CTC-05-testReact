4475c20d0b5ecaea2e0864286d4a072f69454934
JhonyDV <jony.cas998@gmail.com>
2021-02-06 17:09:31 -0300

    test CounterApp

--- src/test/CounterApp.test.js
+++ src/test/CounterApp.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {shallow} from 'enzyme';
-import CounterApp from './CounterApp';
+import CounterApp from '../CounterApp';
 
 describe('Prueba de CounterApp',()=>{
   let  wrapper ;
